dc1f0beefdb2a090587dbeaed63ca5083ee38b80
Myrvelte <myrvelte@gmail.com>
2024-10-28 09:59:21 -0400

    first commit

---
+++ monday.js
@@ -0,0 +1,11 @@
+
+const fruit = ["apple","banana","cherry"]
+
+fruit.forEach(f => console.log(f));
+
+
+[545,333,222].forEach(n => console.log(n));
+
+
+
+["apple","banana","cherry"].forEach(n => console.log(n));
